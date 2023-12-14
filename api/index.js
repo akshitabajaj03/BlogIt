d1cebc4e0147a72727ac38016c86441ee2ab83a1
akshitabajaj03 <akshitabajaj709@gmail.com>
2023-12-14 16:37:16 +0530

    Add existing project files to Git

---
+++ api/index.js
@@ -0,0 +1,167 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+const User= require('./models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({dest:'uploads/'});
+const fs = require('fs');
+const Post = require('./models/Post')
+
+const salt=10;
+const secret = 'adncdncjadncjnccbscbakw';
+
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect('mongodb+srv://akshitabajaj709:KgnrGKOIb5dfc8EZ@cluster0.nhknesb.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register',async (req,res) => {
+    const {username,password} = req.body;
+    try{
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt),
+        });
+        res.json(userDoc);
+    }
+    catch(e){
+       res.status(400).json(e);
+    }
+})
+
+app.post('/login', async(req,res) => {
+   const {username, password} =  req.body;
+   const userDoc = await User.findOne({username:username});
+   const passok = bcrypt.compareSync(password, userDoc.password);
+   if(passok)
+   {
+    jwt.sign({
+        username,
+        id: userDoc._id}, secret , {} , (err, token) => {
+         if(err) throw err;
+         res.cookie('token',token).json({
+            id:userDoc._id,
+            username,
+         });
+        })
+   }
+   else
+   {
+    res.status(400).json('Wrong credentials');
+   }
+})
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+      if (err)
+      {
+      console.log(err);
+      return res.status(401).json({ error: 'Unauthorized' });
+      }
+      res.json(info);
+    });  
+})
+
+app.post('/logout', (req,res) => {
+    res.cookie('token','').json('ok')
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req,res) =>{
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length-1];
+    const newpath = path+'.'+ext;
+    fs.renameSync(path, newpath);
+
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async(err,info) => {
+     if (err) throw err;
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover:newpath,
+        author: info.id,
+    })
+     res.json(postDoc);
+    });
+})
+
+app.get('/post',async (req,res) => {
+   res.json(
+    await Post.find()
+   .populate('author', ['username'])
+   .sort({createdAt:-1})
+   .limit(20)
+)})
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+    let newPath = null;
+    if (req.file) {
+      const {originalname,path} = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      newPath = path+'.'+ext;
+      fs.renameSync(path, newPath);
+    }
+  
+    const {token} = req.cookies;
+
+    jwt.verify(token, secret, {}, async (err,info) => {
+      if (err) throw err;
+      const {id,title,summary,content} = req.body;
+      const postDoc = await Post.findById(id);
+
+      if (!postDoc) {
+        return res.status(404).json('Post not found');
+      }
+
+      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json('you are not the author');
+      }
+
+      /*await Post.update({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+      
+      const updatedPost = await Post.findById(id);
+
+      res.json(updatedPost);*/
+
+      const updatedPost = await Post.findByIdAndUpdate(
+        id,
+        {
+          title,
+          summary,
+          content,
+          cover: newPath ? newPath : postDoc.cover,
+        },
+        { new: true }
+      );
+
+      res.json(updatedPost);
+
+    });
+  
+  });
+  
+
+app.get('/post/:id', async (req, res) => {
+    const {id} = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+  })
+
+app.listen(4000);
